9c5c537a88b21ad64abb305ce87e07c497846d20
ronBoats <roniboat@gmail.com>
2018-06-08 13:44:59 +0000

    button rename

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(80, 38);
+            this.button1.Location = new System.Drawing.Point(87, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "New Button";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -48,6 +48,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
